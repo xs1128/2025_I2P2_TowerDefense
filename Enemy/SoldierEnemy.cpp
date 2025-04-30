041842ef4ec6449cd584f8ce8ba0c686289976d8
Dogeon188 <dogcatdog188@gmail.com>
2025-04-30 17:05:41 +0800

    initial commit

---
+++ Enemy/SoldierEnemy.cpp
@@ -0,0 +1,7 @@
+#include <string>
+
+#include "SoldierEnemy.hpp"
+
+// TODO HACKATHON-3 (1/3): You can imitate the 2 files: 'SoldierEnemy.hpp', 'SoldierEnemy.cpp' to create a new enemy.
+SoldierEnemy::SoldierEnemy(int x, int y) : Enemy("play/enemy-1.png", x, y, 10, 50, 5, 5) {
+}
---
+++ main.cpp
@@ -0,0 +1,26 @@
+// [main.cpp]
+// This is the entry point of your game.
+// You can register your scenes here, and start the game.
+#include "Engine/GameEngine.hpp"
+#include "Engine/LOG.hpp"
+#include "Scene/LoseScene.hpp"
+#include "Scene/PlayScene.hpp"
+#include "Scene/StageSelectScene.hpp"
+#include "Scene/WinScene.hpp"
+#include "Scene/StartScene.h"
+#include "Scene/SettingsScene.hpp"
+
+int main(int argc, char **argv) {
+	Engine::LOG::SetConfig(true);
+	Engine::GameEngine& game = Engine::GameEngine::GetInstance();
+
+    // TODO HACKATHON-2 (2/3): Register Scenes here
+    game.AddNewScene("stage-select", new StageSelectScene());
+	game.AddNewScene("play", new PlayScene());
+	game.AddNewScene("lose", new LoseScene());
+	game.AddNewScene("win", new WinScene());
+
+    // TODO HACKATHON-1 (1/1): Change the start scene
+	game.Start("stage-select", 60, 1600, 832);
+	return 0;
+}
